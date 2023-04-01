9fc0ce36007b141e049b09c0bf7ff9ad3c727f20
ad0lar <jakob_wappler@protonmail.com>
2023-04-02 00:26:05 +0200

    spaghetti mit mtcnn und retinaface

--- data_preparation.py
+++ data_preparation.py
@@ -8,9 +8,6 @@ class dataPreparation():
 
     def __init__(self):
 
-        # video capture object
-        self.vid = cv2.VideoCapture("C:/Users/jakob/Downloads/gkd_4jakob_2023-03-30_1342/4jakob/GX010321.mp4")
-
         self.cascade_face_detector = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
         self.hog_face_detector = cv2.HOGDescriptor()
         self.hog_face_detector.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
@@ -18,67 +15,90 @@ class dataPreparation():
         
         self.count = 0
 
+    def overlapping_area(self, p0 ,p1 ,p2 ,p3):
+        x, y = 0,1
+
+        x_left = max(p0[x], p2[x])
+        y_top = max(p0[y], p2[y])
+        x_right = min(p1[x], p3[x])
+        y_bottom = min(p1[y], p3[y])
+
+        area = 0
+        if x_left <= x_right and y_top <= y_bottom:
+            overlap_area = (x_right-x_left)*(y_bottom-y_top)
+
+            x_left = min(p0[x], p2[x])
+            y_top = min(p0[y], p2[y])
+            x_right = max(p1[x], p3[x])
+            y_bottom = max(p1[y], p3[y])
+
+            max_rect_area = (x_right-x_left)*(y_bottom-y_top)
+            relative_overlapping = overlap_area/max_rect_area
+
+            if(relative_overlapping>0.5): return (x_left,y_top),(x_right,y_bottom)
+            return None
+        return None
+
+
     def get_faces(self,frame):
 
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_faces = {}
+
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         mtcnn_faces = self.mtcnn_face_detector.detect_faces(rgb_frame)
 
         retina_faces = RetinaFace.detect_faces(frame)
 
-        #cascade & hog
-        '''cascade_faces = self.cascade_face_detector.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5)
+        if not isinstance(retina_faces, dict):
+            return frame_faces
 
-        hog_faces, _ = self.hog_face_detector.detectMultiScale(gray_frame)
-        
-        for (x, y, w, h) in cascade_faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        #remove every entry with faces smaller than 95px
+        #retina_faces = {k: v for k, v in retina_faces.items() if v.get("facial_area")[2]-v.get("facial_area")[0]>=95 and v.get("facial_area")[3]-v.get("facial_area")[1]>=95}
+        #mtcnn_faces = [d for d in mtcnn_faces if d.get("box")[2] and d.get("box")[3]>=95]
 
-        for hog_face in hog_faces:
-            (x, y, w, h) = hog_face
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)'''
-
-        for face in mtcnn_faces:
-            #beginning- and endingpoints
-            x, y, w, h = face["box"]
-            print("red:")
-            print(w,h)
-            if(w>=96 and h>=96):
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-
-        if isinstance(retina_faces , dict):
-            for face in retina_faces.keys():
+        if (len(retina_faces) * len(mtcnn_faces) != 0):
+            for face in mtcnn_faces:
                 #beginning- and endingpoints
-                bx, by, ex, ey = retina_faces[face]["facial_area"]
-                print("white:")
-                print((ex-bx),(ey-by))
-                if((ex-bx)>=96 and (ey-by)>=96):
-                    score = round(retina_faces[face]["score"],3)
-                    cv2.rectangle(frame, (ex, ey), (bx, by), (255, 255, 255), 1)
-                    cv2.putText(frame, str(score), (ex, ey), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+                x, y, w, h = face["box"]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    
-    def get_th_frames(self):
+                for face in retina_faces.keys():
+                    #beginning- and endingpoints
+                    bx, by, ex, ey = retina_faces[face]["facial_area"]
+                    cv2.rectangle(frame, (ex, ey), (bx, by), (255, 0, 0), 1)
+                    rectangle = self.overlapping_area((x,y),(x+w,y+h),(bx,by),(ex,ey))
+                    if(not isinstance(rectangle,tuple)):
+                        break
+                    (p1,p2),(p3,p4) = self.overlapping_area((x,y),(x+w,y+h),(bx,by),(ex,ey))
+                    face_name = str(round(self.count/(29.97),3))
+                    while(frame_faces.keys().__contains__(face_name)):
+                        face_name+="Q"
+                    frame_faces[face_name] = ((p1,p2),(p3,p4))
+        return frame_faces
+
+    def store_image(self, frame, frame_faces):
+        for face in frame_faces:
+            (p1, p2), (p3, p4) = frame_faces[face]
+            # Region of Interest
+            roi = frame[p2:p4, p1:p3]
+            cv2.rectangle(frame, (p1, p2), (p3, p4), (255, 255, 255), 1)
+            print(face + ".jpg")
+            cv2.imwrite("C:/Users/jakob/Downloads/gkd_4jakob_2023-03-30_1342/faces/" + face + ".jpg", frame)
+
+    def get_th_frames(self, video_string):
+        self.vid = cv2.VideoCapture(video_string)
 
         while self.vid.isOpened():
             ret, frame = self.vid.read()
             self.count += 1
             if(ret and self.count%5 == 0):
-
-                self.get_faces(frame)
-                        
-                # show frame
-                cv2.imshow('Frame', frame)
-
-            # 'q' as exit nessesary
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
+                print(self.count/29.97)
+                self.store_image(frame, self.get_faces(frame))
 
         # After the loop release the cap object
         self.vid.release()
-        # Destroy all the windows
-        cv2.destroyAllWindows()
 
 dataPreparation1 = dataPreparation()
-dataPreparation1.get_th_frames()
+dataPreparation1.get_th_frames("C:/Users/jakob/Pictures/Camera Roll/WIN_20230401_21_37_45_Pro.mp4")
+#dataPreparation1.get_th_frames("C:/Users/jakob/Downloads/gkd_4jakob_2023-03-30_1342/4jakob/GX010321.mp4")
