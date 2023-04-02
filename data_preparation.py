2b00d88cab3ac9041b772f3289480be7eedb307e
ad0lar <jakob_wappler@protonmail.com>
2023-04-02 04:59:50 +0200

    dfsa

--- data_preparation.py
+++ data_preparation.py
@@ -1,18 +1,25 @@
 import cv2
 import mtcnn
-from retinaface import RetinaFace
-from time import time
-
+import face_detection
+import logging
+import numpy as np
+from models.face_detection_master.face_detection import detector
 
 class dataPreparation():
 
     def __init__(self):
 
-        self.cascade_face_detector = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
-        self.hog_face_detector = cv2.HOGDescriptor()
-        self.hog_face_detector.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        #self.dnn_face_detector = cv2.dnn.readNetFromCaffe("C:/Users/jakob/Documents/private projekte/cam_analysis/models/deploy.prototxt.txt", "C:/Users/jakob/Documents/private projekte/cam_analysis/models/res10_300x300_ssd_iter_140000.caffemodel")
+        self.viola_jones_detector = cv2.CascadeClassifier("C:/Users/jakob/Documents/private projekte/cam_analysis/models/haarcascade_frontalface_alt.xml")
+        #self.retina_face_detector = detector.RetinaFace(gpu_id=-1)
+        #self.cascade_face_detector = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
+        #self.hog_face_detector = cv2.HOGDescriptor()
+        #self.hog_face_detector.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
         self.mtcnn_face_detector = mtcnn.MTCNN()
-        
+        #self.dsfd_detector = face_detection.build_detector("DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
+        #self.retina_net_res_net = face_detection.build_detector("RetinaNetResNet50", confidence_threshold=.5, nms_iou_threshold=.3)
+        self.retina_net_mobile_net = face_detection.build_detector("RetinaNetMobileNetV1", confidence_threshold=.5, nms_iou_threshold=.3)
+
         self.count = 0
 
     def overlapping_area(self, p0 ,p1 ,p2 ,p3):
@@ -24,19 +31,21 @@ class dataPreparation():
         y_bottom = min(p1[y], p3[y])
 
         area = 0
-        if x_left <= x_right and y_top <= y_bottom:
-            overlap_area = (x_right-x_left)*(y_bottom-y_top)
-
-            x_left = min(p0[x], p2[x])
-            y_top = min(p0[y], p2[y])
-            x_right = max(p1[x], p3[x])
-            y_bottom = max(p1[y], p3[y])
-
-            max_rect_area = (x_right-x_left)*(y_bottom-y_top)
-            relative_overlapping = overlap_area/max_rect_area
-
-            if(relative_overlapping>0.5): return (x_left,y_top),(x_right,y_bottom)
+        if x_left >= x_right and y_top >= y_bottom:
             return None
+
+        overlap_area = (x_right-x_left)*(y_bottom-y_top)
+
+        x_left = min(p0[x], p2[x])
+        y_top = min(p0[y], p2[y])
+        x_right = max(p1[x], p3[x])
+        y_bottom = max(p1[y], p3[y])
+
+        max_rect_area = (x_right-x_left)*(y_bottom-y_top)
+        relative_overlapping = overlap_area/max_rect_area
+
+        if(relative_overlapping>0.5): return (x_left,y_top),(x_right,y_bottom)
+
         return None
 
 
@@ -44,37 +53,25 @@ class dataPreparation():
 
         frame_faces = {}
 
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        mtcnn_faces =  self.mtcnn_face_detector.detect_faces(frame)
+        retina_net_mobile_net_faces = self.retina_net_mobile_net.detect(frame).astype(int)
 
-        mtcnn_faces = self.mtcnn_face_detector.detect_faces(rgb_frame)
+        for face in retina_net_mobile_net_faces:
+            x1, y1, x2, y2, score = face
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
-        retina_faces = RetinaFace.detect_faces(frame)
-
-        if not isinstance(retina_faces, dict):
-            return frame_faces
-
-        #remove every entry with faces smaller than 95px
-        #retina_faces = {k: v for k, v in retina_faces.items() if v.get("facial_area")[2]-v.get("facial_area")[0]>=95 and v.get("facial_area")[3]-v.get("facial_area")[1]>=95}
-        #mtcnn_faces = [d for d in mtcnn_faces if d.get("box")[2] and d.get("box")[3]>=95]
-
-        if (len(retina_faces) * len(mtcnn_faces) != 0):
             for face in mtcnn_faces:
-                #beginning- and endingpoints
-                x, y, w, h = face["box"]
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-                for face in retina_faces.keys():
-                    #beginning- and endingpoints
-                    bx, by, ex, ey = retina_faces[face]["facial_area"]
-                    cv2.rectangle(frame, (ex, ey), (bx, by), (255, 0, 0), 1)
-                    rectangle = self.overlapping_area((x,y),(x+w,y+h),(bx,by),(ex,ey))
-                    if(not isinstance(rectangle,tuple)):
-                        break
-                    (p1,p2),(p3,p4) = self.overlapping_area((x,y),(x+w,y+h),(bx,by),(ex,ey))
-                    face_name = str(round(self.count/(29.97),3))
-                    while(frame_faces.keys().__contains__(face_name)):
-                        face_name+="Q"
-                    frame_faces[face_name] = ((p1,p2),(p3,p4))
+                x,y,w,h = face["box"]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+                rect = self.overlapping_area((x,y),(x+w,y+h),(x1,y1),(x2,y2))
+                if (rect == None): break
+                (x1,x2),(x3,x4) = rect
+                cv2.rectangle(frame, (x1, x2), (x3,x4), (255, 255, 255), 1)
+                face_name = str(round(self.count / (29.97), 3))
+                while (frame_faces.keys().__contains__(face_name)):
+                    face_name += "Q"
+                frame_faces[face_name] = ((x1, x2), (x3, x4))
         return frame_faces
 
     def store_image(self, frame, frame_faces):
@@ -82,7 +79,7 @@ class dataPreparation():
             (p1, p2), (p3, p4) = frame_faces[face]
             # Region of Interest
             roi = frame[p2:p4, p1:p3]
-            cv2.rectangle(frame, (p1, p2), (p3, p4), (255, 255, 255), 1)
+            #cv2.rectangle(frame, (p1, p2), (p3, p4), (255, 255, 255), 1)
             print(face + ".jpg")
             cv2.imwrite("C:/Users/jakob/Downloads/gkd_4jakob_2023-03-30_1342/faces/" + face + ".jpg", frame)
 
@@ -93,12 +90,16 @@ class dataPreparation():
             ret, frame = self.vid.read()
             self.count += 1
             if(ret and self.count%5 == 0):
-                print(self.count/29.97)
-                self.store_image(frame, self.get_faces(frame))
-
+                if len(self.get_faces(frame))>0:
+                    cv2.imwrite(f"C:/Users/jakob/Downloads/gkd_4jakob_2023-03-30_1342/faces/{self.count}.jpg", frame)
+                '''cv2.imshow('Frame', frame)
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break'''
         # After the loop release the cap object
         self.vid.release()
 
+print(face_detection.available_detectors)
+
 dataPreparation1 = dataPreparation()
-dataPreparation1.get_th_frames("C:/Users/jakob/Pictures/Camera Roll/WIN_20230401_21_37_45_Pro.mp4")
-#dataPreparation1.get_th_frames("C:/Users/jakob/Downloads/gkd_4jakob_2023-03-30_1342/4jakob/GX010321.mp4")
+#dataPreparation1.get_th_frames("C:/Users/jakob/Pictures/Camera Roll/WIN_20230401_21_37_45_Pro.mp4")
+dataPreparation1.get_th_frames("C:/Users/jakob/Downloads/gkd_4jakob_2023-03-30_1342/4jakob/GX010321.mp4")
