b976c919841a08d7097bee3c191697c02b3366a0
ad0lar <jakob_wappler@protonmail.com>
2023-04-19 16:11:22 +0200

    fd

--- face_classification.py
+++ face_classification.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
-import openface
+import json
 from deepface import DeepFace
+from keras.layers import Dense
 from keras_facenet import FaceNet
 import os
 import sklearn.cluster as cl
@@ -10,9 +11,10 @@ from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_har
 from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
 
-model = VGGFace(model='senet50', include_top=True, input_shape=(224, 224, 3), pooling='max')
-model1 = DeepFace.build_model('Facenet')
-# TODO: combine VGGFace and FACEnet
+model = VGGFace(model='senet50', include_top=True, input_shape=(224, 224, 3))
+model1 = DeepFace.build_model('DeepID')
+model2 = DeepFace.build_model('DeepFace')
+model3 = DeepFace.build_model('OpenFace')
 
 class FaceClassification():
 
@@ -20,21 +22,30 @@ class FaceClassification():
         self.facenet = FaceNet()
         self.embedding_data = {}
 
-    # extract faces and calculate face embeddings for a list of photo files
-    def get_vgg_embedding(self, img):
-        # extract faces
-        img = cv2.resize(img,(224,224))
+    def preprocess_img(self,img, shape):
+        img = cv2.resize(img,shape)
         img = asarray(img, 'float32')
         img = expand_dims(img, axis=0)
-        # prepare the face for the model, e.g. center pixels
-
         img = preprocess_input(img, version=2)
-        # perform prediction
-        embedding = model.predict(img, verbose=False)
-        return embedding
+        return img
+    # extract faces and calculate face embeddings for a list of photo files
+    def get_vgg_embedding(self, img):
+        embedding_DeepID = model1.predict(self.preprocess_img(img, (47, 55)), verbose=False)
+        embedding_DeepID = embedding_DeepID / np.linalg.norm(np.array(embedding_DeepID).flatten(), axis=0,keepdims=True)
+
+        embedding_DeepFace = model2.predict(self.preprocess_img(img, (152, 152)), verbose=False)
+        embedding_DeepFace = embedding_DeepFace / np.linalg.norm(np.array(embedding_DeepFace).flatten(), axis=0,keepdims=True)
+
+        embedding_OpenFace = model3.predict(self.preprocess_img(img, (96, 96)), verbose=False)
+        embedding_OpenFace = embedding_OpenFace / np.linalg.norm(np.array(embedding_OpenFace).flatten(), axis=0,keepdims=True)
+
+        three_way_embed = np.concatenate((embedding_DeepFace,embedding_OpenFace,embedding_DeepID), axis=1)
+
+
+        return three_way_embed
 
     def get_deepface_embedding(self, img):
-        img = cv2.resize(img,(160,160))
+        img = cv2.resize(img,(47, 55))
         img = asarray(img, 'float32')
         img = expand_dims(img, axis=0)
         embedding = model1.predict(img,verbose=False)
@@ -51,18 +62,24 @@ class FaceClassification():
                         embedding1 = self.get_vgg_embedding(img_data)
                         embedding2 = self.get_deepface_embedding(img_data)
                         if embedding1 is not None and embedding2 is not None:
+                            embedding1 = np.array(embedding1).flatten() / np.linalg.norm(np.array(embedding1).flatten(), axis=0, keepdims=True)
+                            embedding2 = np.array(embedding2).flatten() / np.linalg.norm(np.array(embedding2).flatten(), axis=0, keepdims=True)
                             self.embedding_data[count] = {"img_name": face_img,
                                                           "video_source": video_name,
                                                           "img_data": cv2.resize(img_data, (160,160)),
                                                           "path": img_path,
-                                                          "embedding":np.concatenate((np.array(embedding1).flatten(),np.array(embedding2).flatten()))}
+                                                          "embedding":np.concatenate((embedding1,embedding2))}
                             count += 1
                     except cv2.error:
                         print("Invalid frame!")
         img_data_list = [self.embedding_data[img]["img_data"] for img in self.embedding_data]
         embeddings = self.facenet.embeddings(img_data_list)
         for embed in range(len(embeddings)):
-            self.embedding_data[embed]["embedding"] = np.concatenate((np.array(embeddings[embed]), np.array(self.embedding_data[embed]["embedding"]).flatten()))
+            embedding1 = np.array(embeddings[embed])
+            embedding1 = embedding1/ np.linalg.norm(embedding1, axis=0, keepdims=True)
+
+            embedding = np.concatenate((embedding1, self.embedding_data[embed]["embedding"]))
+            self.embedding_data[embed]["embedding"] = embedding
         return self.embedding_data
 
     def get_classes(self):
@@ -71,8 +88,6 @@ class FaceClassification():
         X = X.reshape(X.shape[0],-1)
         #X = StandardScaler().fit_transform(X)
         # Erstelle das Dictionary der Hyperparameter-Werte
-        '''for eps_ in [0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.72,0.74,0.76,0.78,0.8,0.85,0.9]:
-        labels = self.calculate_k(X, eps = eps_)'''
         label_list = self.calculate_labels(X)
         for eps_ in label_list:
             labels = label_list[eps_]
@@ -98,24 +113,24 @@ class FaceClassification():
                 f = open(save_dir + "info.txt", "a")
                 first_source = labels_to_img[label][0]["video_source"]
                 for img in labels_to_img[label]:
-                    cv2.imwrite(save_dir + "/" + img["img_name"], img["img_data"])
+                    cv2.imwrite(save_dir + "/" + img["img_name"], np.array(img["img_data"]))
                     f.write(img["img_name"] + "       video : " + img["video_source"] + "     timestamp : " +
                             img["img_name"].rsplit('.', 1)[0] + " sec\n")
                     if img["video_source"] is not first_source:
                         different_sources = True
                 f.close()
     def calculate_k(self, X):
-        max_k = {"labels":cl.KMeans(n_clusters=2).fit_predict(), "score":None}
+        max_k = {}
         for k in range(2, len(X)-1):
             kmeans = cl.KMeans(n_clusters=k)
             clusters = kmeans.fit_predict(X)
-            silhouette_avg = silhouette_score(X, clusters)
+            '''silhouette_avg = silhouette_score(X, clusters)
 
             # Calinski-Harabasz Score berechnen
             ch_score = calinski_harabasz_score(X, clusters)
-            score = silhouette_avg*ch_score
-            if max_k["score"] is None or max_k["score"] < score:
-                max_k = {"labels":clusters, "score":score}
+            score = silhouette_avg*ch_score'''
+
+            max_k[k] = clusters
         return max_k
 
     def calculate_labels(self,X):
@@ -137,7 +152,7 @@ class FaceClassification():
 
             score = silhouette_avg * ch_score
 
-            print(eps, score, unique, clusters)
+            print(eps, silhouette_avg,ch_score,score, unique)
 
             max_k[eps] = clusters
             eps += 0.1
