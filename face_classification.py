93329e03c39c837ca579c80944c99d78fda60e10
ad0lar <jakob_wappler@protonmail.com>
2023-04-19 17:14:35 +0200

    fs

--- face_classification.py
+++ face_classification.py
@@ -12,11 +12,7 @@ from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
 
 model = VGGFace(model='senet50', include_top=True, input_shape=(224, 224, 3))
-model1 = DeepFace.build_model('DeepID')
-model2 = DeepFace.build_model('DeepFace')
-model3 = DeepFace.build_model('OpenFace')
-model4 = DeepFace.build_model('VGG-Face')
-model5 = DeepFace.build_model('Facenet')
+model_list = [model, DeepFace.build_model('DeepID'),DeepFace.build_model('DeepFace'),DeepFace.build_model('OpenFace'),DeepFace.build_model('VGG-Face'),DeepFace.build_model('Facenet')]
 
 class FaceClassification():
 
@@ -34,7 +30,6 @@ class FaceClassification():
     def get_some_embeddings(self, img):
         multiple_way_embed = None
         shape_list = [(224, 224),(47, 55), (152, 152),(96, 96), (224, 224), (160, 160)]
-        model_list = [model, model1, model2, model3, model4, model5]
         for model_no in range(len(model_list)):
             embedding = model_list[model_no].predict(self.preprocess_img(img, shape_list[model_no]), verbose=False)
             embedding = embedding / np.linalg.norm(np.array(embedding).flatten(), axis=0,keepdims=True)
@@ -79,73 +74,44 @@ class FaceClassification():
         X = X.reshape(X.shape[0],-1)
         #X = StandardScaler().fit_transform(X)
         # Erstelle das Dictionary der Hyperparameter-Werte
-        label_list = self.calculate_labels(X)
-        for eps_ in label_list:
-            labels = label_list[eps_]
-            labels_to_img = {}
-            for unique_label in np.unique(labels):
-                labels_to_img[str(unique_label)] = []
-            for data_point in range(len(labels)):
-                labels_to_img[str(labels[data_point])].append(self.embedding_data[data_point])
+        labels = self.calculate_labels(X)
+        labels_to_img = {}
+        for unique_label in np.unique(labels):
+            labels_to_img[str(unique_label)] = []
+        for data_point in range(len(labels)):
+            labels_to_img[str(labels[data_point])].append(self.embedding_data[data_point])
 
-            out_saving_path = "result/clustered_" + "EPS_" + str(round(eps_,3))
-            if not os.path.isdir(out_saving_path):
-                os.mkdir(out_saving_path)
+        out_saving_path = "result/labeled/"
+        if not os.path.isdir(out_saving_path):
+            os.mkdir(out_saving_path)
 
-            for label in labels_to_img:
-                different_sources = False
-                save_dir = out_saving_path + "/" + str(label) +"/"
-                if not os.path.isdir(save_dir):
-                    os.mkdir(save_dir)
-                for face in labels_to_img[label]:
-                    cv2.imwrite(save_dir+"/"+face["img_name"],face["img_data"])
-                f = open(save_dir + "info.txt", "x")
+        for label in labels_to_img:
+            different_sources = False
+            save_dir = out_saving_path + str(label) +"/"
+            if not os.path.isdir(save_dir):
+                os.mkdir(save_dir)
+
+            f = open(save_dir + "info.txt", "x")
+            f.close()
+            f = open(save_dir + "info.txt", "a")
+            first_source = labels_to_img[label][0]["video_source"]
+
+            for face in labels_to_img[label]:
+                cv2.imwrite(save_dir+face["img_name"],face["img_data"])
+                f.write(face["img_name"] + "       video : " + face["video_source"] + "     timestamp : " +
+                        face["img_name"].rsplit('.', 1)[0] + " sec\n")
+                if face["video_source"] is not first_source:
+                    different_sources = True
+            f.close()
+            if different_sources:
+                f = open("result/multiple_sources.txt", "w")
                 f.close()
-                f = open(save_dir + "info.txt", "a")
-                first_source = labels_to_img[label][0]["video_source"]
-                for img in labels_to_img[label]:
-                    cv2.imwrite(save_dir + "/" + img["img_name"], np.array(img["img_data"]))
-                    f.write(img["img_name"] + "       video : " + img["video_source"] + "     timestamp : " +
-                            img["img_name"].rsplit('.', 1)[0] + " sec\n")
-                    if img["video_source"] is not first_source:
-                        different_sources = True
+                f = open("result/multiple_sources.txt", "a")
+                f.write("face label nr.:" + str(label) + "\n")
                 f.close()
-    def calculate_k(self, X):
-        max_k = {}
-        for k in range(2, len(X)-1):
-            kmeans = cl.KMeans(n_clusters=k)
-            clusters = kmeans.fit_predict(X)
-            '''silhouette_avg = silhouette_score(X, clusters)
-
-            # Calinski-Harabasz Score berechnen
-            ch_score = calinski_harabasz_score(X, clusters)
-            score = silhouette_avg*ch_score'''
-
-            max_k[k] = clusters
-        return max_k
 
     def calculate_labels(self,X):
-        max_k = {}
-        # Berechne den Silhouetten-Score für k-Werte zwischen 2 und 10
-        eps = 0.1
-        while len(np.unique(cl.DBSCAN(eps=eps).fit_predict(X))) == 1:
-            eps+=0.1
-            print(eps, len(np.unique(cl.DBSCAN(eps=eps).fit_predict(X))))
-        unique = len(np.unique(cl.DBSCAN(eps=eps).fit_predict(X)))
-        while 1 < unique < len(X):
-            dbscan = cl.DBSCAN(eps=eps)
-            clusters = dbscan.fit_predict(X)
-
-            silhouette_avg = silhouette_score(X, clusters)
-
-            # Calinski-Harabasz Score berechnen
-            ch_score = calinski_harabasz_score(X, clusters)
-
-            score = silhouette_avg * ch_score
-
-            print(eps, silhouette_avg,ch_score,score, unique)
-
-            max_k[eps] = clusters
-            eps += 0.1
-            unique = len(np.unique(cl.DBSCAN(eps=eps).fit_predict(X)))
-        return max_k
+        eps = 1.1
+        dbscan = cl.DBSCAN(eps=eps)
+        clusters = dbscan.fit_predict(X)
+        return clusters
